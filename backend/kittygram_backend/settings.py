267c8d9715f31fb516dcdbedd59f8cae0a2f7963
Peter Liepa <peterliepa@MacBook-Pro-Peter.local>
2024-05-21 12:48:22 +0300

    finaly 4

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -14,7 +14,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypeπs',
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
